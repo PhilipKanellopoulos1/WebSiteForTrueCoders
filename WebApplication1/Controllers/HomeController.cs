e28329882102f55fcb185b8d935aa847983fb7d1
Philip Kanellopoulos <pgekanel01@gmail.com>
2019-07-31 12:59:25 -0500

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebsiteForTrueCoders;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller //inheriting from class controller where View is a method in that class. it is set up by using microsoft.aspnetcore.mvc
+    {
+        public IActionResult Index()
+        {
+            ProductRepository repo = new ProductRepository();
+            List<Product> allProducts = repo.GetAllProducts();
+
+            return View(allProducts);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WebsiteForTrueCoders
+{
+    class ProductRepository
+    {
+        public List<Product> GetAllProducts()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ProductID, Name, Price FROM products;";
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                List<Product> products = new List<Product>();
+
+                while (reader.Read())
+                {
+                    Product nextProduct = new Product();
+
+                    nextProduct.Id = reader.GetInt32("ProductID");
+                    nextProduct.Name = reader.GetString("Name");
+                    nextProduct.Price = reader.GetDecimal("Price");
+
+                    products.Add(nextProduct);
+                }
+
+                return products;
+
+            }
+        }
+    }
+}
